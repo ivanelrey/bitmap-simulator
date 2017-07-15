f0fda9e36c70404e9a399fcc423ea0e880b2c7e3
ivane gomarteli <ivanejg@yahoo.com>
2017-07-15 19:57:32 +0300

    Create test.txt's for spec examples on the go and delete it when finished

--- spec/file_manager_spec.rb
+++ spec/file_manager_spec.rb
@@ -3,7 +3,13 @@ require './lib/file_manager.rb'
 
 describe FileManager do |example|
 
-	it "initializes init variables" do 
+	after do
+		unless RSpec.current_example.metadata[:skip_before]		
+ 			File.delete("./spec/test.txt")		
+ 		end
+  	end
+
+	it "initializes init variables", :skip_before do 
 		@f= FileManager.new("./examples/show.txt")
 		expect(@f.file).to eq("./examples/show.txt")
 		expect(@f.init_colour).to eq("O")
@@ -18,74 +24,137 @@ describe FileManager do |example|
 	end
 
 	it "stops reading the file because the first command is not 'I'(to initialize the bitmap)" do 
-		@f= FileManager.new("./spec/spec_examples/the_first_cmd_not_init.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("L 3 5")
+		out_file.puts("I 5 6")
+		out_file.puts("S")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expect(@f.errors_found_in_file[0]).to eq("First command must be 'I' to initialize the bitmap.")
 	end
 
 	it "adds error if lower case command found" do 
-		@f= FileManager.new("./spec/spec_examples/contains_lower_case_cmd.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("I 5 6")
+		out_file.puts("l 1 3 A")
+		out_file.puts("S")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expect(@f.errors_found_in_file[0]).to eq("ERROR in line 2: A command must be a capital letter.")
 	end
 
 	it "adds error if unrecognized command found" do 
-		@f= FileManager.new("./spec/spec_examples/contains_unknown_cmd.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("I 5 6")
+		out_file.puts("D 1 3 A")
+		out_file.puts("S")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expect(@f.errors_found_in_file[0]).to eq("ERROR in line 2: Unrecognised command :(.")
 	end
 
 	it "adds error if params size for init command is not correct" do 
-		@f= FileManager.new("./spec/spec_examples/wrong_size_of_params.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("I 5")
+		out_file.puts("D 1 3 A")
+		out_file.puts("S")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expect(@f.errors_found_in_file[0]).to eq("ERROR in line 1: Number of parameteres for this command is 2 ")
 	end
 
 	it "adds error if param is out of min - max range" do 
-		@f= FileManager.new("./spec/spec_examples/param_out_of_range.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("I 5 300")
+		out_file.puts("D 1 3 A")
+		out_file.puts("S")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expect(@f.errors_found_in_file[0]).to eq("ERROR in line 1: Wrong param '300'. The current param must be in range (1 .. 250)")
 	end
 
 	it "adds error if pixel param is not integer" do 
-		@f= FileManager.new("./spec/spec_examples/pixel_param_is_not_integer.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("I 5 S")
+		out_file.puts("L 1 3 A")
+		out_file.puts("S")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expect(@f.errors_found_in_file[0]).to eq("ERROR in line 1: Wrong param 'S'.The current param must be an integer. ")
 	end
 
 	it "adds error if colour param is not from 'A' to 'Z'" do 
-		@f= FileManager.new("./spec/spec_examples/colour_param_is_not_valid.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("I 5 6")
+		out_file.puts("L 1 3 ss")
+		out_file.puts("S")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expect(@f.errors_found_in_file[0]).to eq("ERROR in line 2: Wrong param 'ss'.Param must be a Capital letter (A .. Z).")
 	end
 
 	it "adds error if vertical segment command has wrong params" do 
-		@f= FileManager.new("./spec/spec_examples/vertical_cmd_wrong_params.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("I 5 6")
+		out_file.puts("L 1 3 A")
+		out_file.puts("V 1 3 A")
+		out_file.puts("S")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expect(@f.errors_found_in_file[0]).to eq("ERROR in line 3: Number of parameteres for this command is 4 ")
 	end
 
 	it "adds error if horizontal segment command has wrong colour params" do 
-		@f= FileManager.new("./spec/spec_examples/horizontal_cmd_wrong_colour_param.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("I 5 6")
+		out_file.puts("L 1 3 A")
+		out_file.puts("V 2 3 6 W")
+		out_file.puts("H 3 5 2 5")
+		out_file.puts("S")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expect(@f.errors_found_in_file[0]).to eq("ERROR in line 4: Wrong param '5'.Param must be a Capital letter (A .. Z).")
 	end
 
 	it "adds errors that command clear and show has wrong params" do 
-		@f= FileManager.new("./spec/spec_examples/clear_show_cmds_wrong_params.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("I 5 6")
+		out_file.puts("L 1 3 A")
+		out_file.puts("V 2 3 6 W")
+		out_file.puts("H 3 5 2 F")
+		out_file.puts("C 4")
+		out_file.puts("S 2")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expect(@f.errors_found_in_file[0]).to eq("ERROR in line 5: Number of parameteres for this command is 0 ")
 		expect(@f.errors_found_in_file[1]).to eq("ERROR in line 6: Number of parameteres for this command is 0 ")
 	end
 
 	it "should not have any errors" do
-		@f= FileManager.new("./spec/spec_examples/correct_commands.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("I 5 6")
+		out_file.puts("L 1 3 A")
+		out_file.puts("V 2 3 6 W")
+		out_file.puts("H 3 5 2 Z")
+		out_file.puts("S")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expect(@f.errors_found_in_file).to be_empty
 	end
 
 	it "deletes empty lines from the file" do 
-		out_file = File.new("./spec/spec_examples/odd_empty_lines.txt", "w+")
+		out_file = File.new("./spec/test.txt", "w+")
 		out_file.puts("    \n")
 		out_file.puts("\n")
 		out_file.puts("I 5 6")
@@ -94,14 +163,22 @@ describe FileManager do |example|
 		out_file.puts("    \n")
 		out_file.puts("S")
 		out_file.close
-		@f= FileManager.new("./spec/spec_examples/odd_empty_lines.txt")
+		@f= FileManager.new("./spec/test.txt")
 		@f.remove_blank_lines_from_file
 		count = IO.readlines(@f.file).size  
 		expect(count).to eq(2) 
 	end
 
 	it "prints the errors" do 
-		@f= FileManager.new("./spec/spec_examples/clear_show_cmds_wrong_params.txt")
+		out_file = File.new("./spec/test.txt", "w+")
+		out_file.puts("I 5 6")
+		out_file.puts("L 1 3 A")
+		out_file.puts("V 2 3 6 W")
+		out_file.puts("H 3 5 2 F")
+		out_file.puts("C 4")
+		out_file.puts("S 2")
+		out_file.close
+		@f= FileManager.new("./spec/test.txt")
 		@f.read_file
 		expected_errors = "ERROR in line 5: Number of parameteres for this command is 0 \nERROR in line 6: Number of parameteres for this command is 0 \n"
 		expect { @f.show_errors }.to output(expected_errors).to_stdout
