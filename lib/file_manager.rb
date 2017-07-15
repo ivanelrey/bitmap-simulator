c38d55a91eeae3abd15ca43ca77ced7c378b6a9e
ivane gomarteli <ivanejg@yahoo.com>
2017-07-15 12:18:46 +0300

    add settings.json and load init data from it

--- lib/file_manager.rb
+++ lib/file_manager.rb
@@ -1,29 +1,27 @@
 class FileManager 
+	require 'json'
 	attr_accessor :init_colour, :init_bitmap, :colour_pixel, :draw_vertical_segment,
                   :draw_horizontal_segment, :show_command, :clear, :max_rows, :max_columns, :file
 
 	def initialize(file)
 		@file = file
-		@init_bitmap = Settings.fetch("init_bitmap")
-	    @clear = Settings.fetch("clear")
-	    @colour_pixel = Settings.fetch("colour_pixel")
-	    @draw_vertical_segment = Settings.fetch("draw_vertical_segment")
-	    @draw_horizontal_segment = Settings.fetch("draw_horizontal_segment")
-	    @show_command = Settings.fetch("show_command")
-	    @init_colour = Settings.fetch("init_colour")
-	    @max_columns = Settings.fetch("max_columns")
-	    @max_rows = Settings.fetch("max_rows")
+		init_settings
 	end
 
-	Settings = {
-	  "init_bitmap" => "I",
-	  "clear" => "C",
-	  "colour_pixel" => "L",
-	  "draw_vertical_segment" => "V",
-	  "draw_horizontal_segment" => "H",
-	  "show_command" => "S",
-	  "init_colour" => "O",
-	  "max_rows" => 250,
-	  "max_columns" => 250
-	}.freeze
+	def init_settings
+	    file = File.read ("./lib/settings.json")
+	    if file.nil?
+	      puts "Please provide correct settings.json file."
+	    end
+	    data = JSON.parse(file)
+	    @init_bitmap = data["init_bitmap"]
+	    @clear = data["clear"]
+	    @colour_pixel = data["colour_pixel"]
+	    @draw_vertical_segment = data["draw_vertical_segment"]
+	    @draw_horizontal_segment = data["draw_horizontal_segment"]
+	    @show_command = data["show_command"]
+	    @init_colour = data["init_colour"]
+	    @max_columns = data["max_columns"]
+    	@max_rows = data["max_rows"]
+  	end
 end
